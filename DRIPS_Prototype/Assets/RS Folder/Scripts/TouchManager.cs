ceb25f467f48bc5581b8c5f6023e642e2d17b9d5
Roberto <rs9864p@gre.ac.uk>
2025-10-05 13:47:01 +0100

    Commented TouchManager Script

--- DRIPS_Prototype/Assets/RS Folder/Scripts/TouchManager.cs
+++ DRIPS_Prototype/Assets/RS Folder/Scripts/TouchManager.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class TouchManager : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    [SerializeField] private float playerMovSpeed;
+    [SerializeField] private GameObject player;       // The player object that will move
+    [SerializeField] private float playerMovSpeed;    // Speed at which the player moves
 
-    private PlayerInput playerInput;
+    private PlayerInput playerInput;                  // Reference to PlayerInput component
 
-    private InputAction touchPositionAction;
-    private Vector3 position;
-    private bool moving;
+    private InputAction touchPositionAction;          // Action to read screen touch position
+    private InputAction touchPressAction;             // Action to detect when the screen is pressed
 
-    private InputAction touchPressAction;
+    private Vector3 position;                         // Target world position where player should move
+    private bool moving;                              // Flag to check if the player is currently moving
 
     private void Awake()
     {
+        // Get PlayerInput component and cache actions
         playerInput = GetComponent<PlayerInput>();
         if (playerInput != null)
         {
@@ -22,12 +24,16 @@ public class TouchManager : MonoBehaviour
             touchPressAction = playerInput.actions["TouchPress"];
         }
     }
+
     private void Update()
     {
+        // If player is flagged as moving, move towards the target position
         if (moving)
         {
+            // Move player towards target smoothly at defined speed
             player.transform.position = Vector3.MoveTowards(player.transform.position, position, playerMovSpeed * Time.deltaTime);
 
+            // Stop moving when close enough to the target
             if (Vector3.Distance(player.transform.position, position) < 0.01f)
             {
                 moving = false;
@@ -37,25 +43,33 @@ public class TouchManager : MonoBehaviour
 
     private void OnEnable()
     {
+        // Subscribe to touch press input
         touchPressAction.performed += TouchPressed;
     }
 
     private void OnDisable()
     {
+        // Unsubscribe when object is disabled
         touchPressAction.performed -= TouchPressed;
     }
 
     private void TouchPressed(InputAction.CallbackContext context)
     {
+        // Get the touch position from input (screen space)
         Vector2 touchedPosition = touchPositionAction.ReadValue<Vector2>();
+
+        // Create a ray from the camera through the touched screen point
         Ray ray = Camera.main.ScreenPointToRay(touchedPosition);
+
+        // Define a horizontal plane at the player's Y position
         Plane groundPlane = new Plane(Vector3.up, new Vector3(0, player.transform.position.y, 0));
-        
+
         float enter;
+        // If the ray hits the plane, set that point as the new target position
         if (groundPlane.Raycast(ray, out enter))
         {
-            position = ray.GetPoint(enter);
-            moving = true;
+            position = ray.GetPoint(enter);   // Get the intersection point in world space
+            moving = true;                    // Start moving the player
         }
     }
 }
